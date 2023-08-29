11494e5ef3aee157a7fb21ca19a9e4539c7fd753
Vlad Huivyk <puchok80@gmail.com>
2023-08-30 00:56:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,99 +1,102 @@
-# main.py
-
-import game_logic
-import openai_integration
 import telebot
-from telebot import types
+import openai
 
-# Ваш API-ключ бота від BotFather
+# Конфігурації
 BOT_TOKEN = '6504314249:AAHqLEfvAXlP76l0AqxGvZ6MNmhw36YIP0o'
+OPENAI_API_KEY = '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'
 
 bot = telebot.TeleBot(BOT_TOKEN)
+openai.api_key = OPENAI_API_KEY
 
-# Створення клавіатури для вибору фракції
-def create_faction_keyboard():
-    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton('Обовʼязок'))
-    keyboard.add(types.KeyboardButton('Вільні Сталкери'))
-    keyboard.add(types.KeyboardButton('Торговці'))
-    keyboard.add(types.KeyboardButton('Свобода'))
-    return keyboard
+class Player:
+    def __init__(self, user_id):
+        self.user_id = user_id
+        self.name = None
+        self.faction = None
+        self.story_context = []
+        self.available_choices = []
+        self.awaiting_choice = False
 
-# Обробник команди /start
+players = {}
+
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = str(message.from_user.id)
-    game_logic.players[user_id] = {'state': 'choosing_name'}
-    bot.reply_to(message, "Ласкаво просимо до гри 'Standalone'!\nБудь ласка, виберіть ім'я свого персонажа:")
+    players[user_id] = Player(user_id)
+    bot.send_message(message.chat.id, "Ласкаво просимо до гри 'Standalone'!\nБудь ласка, назвіть свого персонажа:")
 
-# Обробник текстових повідомлень
-@bot.message_handler(func=lambda message: True)
-def handle_text_message(message):
+@bot.message_handler(func=lambda message: players.get(str(message.from_user.id)) and not players[str(message.from_user.id)].name)
+def set_name(message):
     user_id = str(message.from_user.id)
-    player_data = game_logic.players.setdefault(user_id, {})
-    user_input = message.text
+    players[user_id].name = message.text
+    factions = ["Свобода", "Долина", "Оселедець", "Моноліт", "Бандити", "Науковці", "Вільні Сталкери", "Торговці", "Обов'язок", "Мутанти"]
+    factions_text = "\n".join([f"{i + 1}. {faction}" for i, faction in enumerate(factions)])
+    bot.send_message(message.chat.id, f"{players[user_id].name}, тепер виберіть фракцію, до якої ваш персонаж буде належати:\n{factions_text}")
 
-    if player_data.get('state') == 'choosing_name':
-        player_data['name'] = user_input
-        player_data['state'] = 'choosing_faction'
-        bot.reply_to(message, f"Добре, ви обрали ім'я: {user_input}\nТепер будь ласка, виберіть фракцію:",
-                     reply_markup=create_faction_keyboard())
-    elif player_data.get('state') == 'choosing_faction':
-        player_data['faction'] = user_input
-        player_data['state'] = 'playing'
-        bot.reply_to(message, f"Ви обрали фракцію: {user_input}\nПочинаємо генерацію сюжету гри!")
-
-        # Генерація першого блоку сюжету та виведення його користувачу
-        response = openai_integration.generate_story_block(player_data)
-        bot.reply_to(message, response)
-
-# Реагування на кнопки вибору дії
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def handle_start_button(message):
+@bot.message_handler(func=lambda message: players.get(str(message.from_user.id)) and not players[str(message.from_user.id)].faction)
+def set_faction(message):
     user_id = str(message.from_user.id)
-    player_data = game_logic.players.setdefault(user_id, {})
-    user_input = message.text
+    players[user_id].faction = message.text
+    bot.send_message(message.chat.id, f"{players[user_id].name}, ви обрали фракцію: {players[user_id].faction}\nПочинаємо генерацію сюжету гри!\n"
+                                      f"Натисніть кнопку 'Поїхали', щоб розпочати гру.")
 
-    if player_data.get('state') == 'waiting_for_start' and user_input == 'Поїхали':
-        player_data['state'] = 'playing'
-        bot.reply_to(message, "Починаємо генерацію сюжету гри!")
-
-        # Генерація першого блоку сюжету та виведення його користувачу
-        response = openai_integration.generate_story_block(player_data)
-        bot.reply_to(message, response)
-
-        # Вивід варіантів дій
-        options = game_logic.get_current_options(player_data)
-        options_text = "\n".join([f"{index + 1}. {option}" for index, option in enumerate(options)])
-        bot.reply_to(message, f"Що ви будете робити?\n{options_text}")
-
-
-@bot.message_handler(func=lambda message: True, content_types=['text'])
-def handle_action_choice(message):
+@bot.message_handler(func=lambda message: players.get(str(message.from_user.id)) and message.text == "Поїхали")
+def start_game(message):
     user_id = str(message.from_user.id)
-    player_data = game_logic.players.setdefault(user_id, {})
-    user_input = message.text
+    player = players[user_id]
+    player.awaiting_choice = True
+    generate_story(player)
 
-    if player_data.get('state') == 'playing' and user_input.isdigit():
-        action_index = int(user_input) - 1
-        options = game_logic.get_current_options(player_data)
-        if 0 <= action_index < len(options):
-            player_data['state'] = 'playing'
-            response = openai_integration.generate_story_block(player_data, action_choice=options[action_index])
-            bot.reply_to(message, response)
+@bot.message_handler(func=lambda message: players.get(str(message.from_user.id)) and players[str(message.from_user.id)].awaiting_choice)
+def handle_choice(message):
+    user_id = str(message.from_user.id)
+    player = players[user_id]
+    
+    if message.text in player.available_choices:
+        choice = message.text
+        player.story_context.append(choice)
+        generate_story(player)
+    else:
+        bot.send_message(message.chat.id, "Будь ласка, оберіть один із доступних варіантів.")
 
-            # Вивід нових варіантів дій
-            new_options = game_logic.get_current_options(player_data)
-            new_options_text = "\n".join([f"{index + 1}. {option}" for index, option in enumerate(new_options)])
-            bot.reply_to(message, f"Що ви будете робити далі?\n{new_options_text}")
+def generate_story(player):
+    context = "\n".join(player.story_context)
+    
+    if player.faction == "Свобода":
+        prompt = f"Гравець {player.name} з фракції Свобода. В минулому він {context}."
+    elif player.faction == "Долина":
+        prompt = f"Гравець {player.name} з фракції Долина. В минулому він {context}."
+    elif player.faction == "Оселедець":
+        prompt = f"Гравець {player.name} з фракції Оселедець. В минулому він {context}."
+    elif player.faction == "Моноліт":
+        prompt = f"Гравець {player.name} з фракції Моноліт. В минулому він {context}."
+    elif player.faction == "Бандити":
+        prompt = f"Гравець {player.name} з фракції Бандити. В минулому він {context}."
+    elif player.faction == "Науковці":
+        prompt = f"Гравець {player.name} з фракції Науковці. В минулому він {context}."
+    elif player.faction == "Вільні Сталкери":
+        prompt = f"Гравець {player.name} з фракції Вільні Сталкери. В минулому він {context}."
+    elif player.faction == "Торговці":
+        prompt = f"Гравець {player.name} з фракції Торговці. В минулому він {context}."
+    elif player.faction == "Обов'язок":
+        prompt = f"Гравець {player.name} з фракції Обов'язок. В минулому він {context}."
+    elif player.faction == "Мутанти":
+        prompt = f"Гравець {player.name} - мутант. В минулому він {context}."
+    
+    gpt_response = generate_gpt_response(prompt)
+    choices = generate_action_options(player.available_choices)
+    bot.send_message(player.user_id, f"{gpt_response}\n\nЩо ви будете робити?\n{choices}")
 
+    
+    gpt_response = generate_gpt_response(prompt)
+    choices = generate_action_options(player.available_choices)
+    bot.send_message(player.user_id, f"{gpt_response}\n\nЩо ви будете робити?\n{choices}")
+    
+def generate_gpt_response(prompt):
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt=prompt,
 
-def main():
-    bot.polling(none_stop=True)
-
-if __name__ == "__main__":
-    main()
-
-
-
-
+def generate_action_options(options):
+    formatted_options = "\n".join([f"{i+1}. {option}" for i, option in enumerate(options)])
+    return formatted_options
