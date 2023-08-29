fa057b0d6da9e48cdcb30465916eb918827e9abd
Vlad Huivyk <puchok80@gmail.com>
2023-08-29 16:55:22 +0300

    Create character.py

---
+++ python/character.py
@@ -0,0 +1,7 @@
+class Character:
+    def __init__(self, name, faction):
+        self.name = name
+        self.faction = faction
+        self.inventory = []
+
+    # Add methods for character actions, inventory management, etc.
